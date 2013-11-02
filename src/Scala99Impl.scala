7a3025dbb003da21196fe5797d76684b236ad965
poe <zhpooer@gmail.com>
2013-11-02 21:09:12 +0800

    P07

--- src/Scala99Impl.scala
+++ src/Scala99Impl.scala
@@ -14,4 +14,31 @@ object Scala99Impl {
   }
 
 
+  def nth[T](index:Int, seq:Seq[T]):T = (index, seq) match {
+    case (0, a :: _) => a
+    case (n, _ :: b) => nth( n-1, b )
+    case _ => throw new NoSuchElementException
+  }
+
+  def length[T](seq:Seq[T]):Int = seq match {
+    case a :: Nil => 1
+    case _ :: b   => length(b) + 1
+  }
+
+  def reverse[T](seq:List[T]):List[T] = seq match {
+    case a :: Nil => List(a)
+    case a :: b => reverse(b) ::: List(a)
+    case Nil => Nil
+  }
+
+  def isPalindrome[T](seq:Seq[T]):Boolean = seq.reverse == seq
+
+  def flatten(seq:Seq[Any]):Seq[Any] = seq.flatMap {
+    case a:Seq[_] => flatten(a)
+    case b => List(b)
+  }
+
+
+
 }
+
--- test/src/Scala99Spec.scala
+++ test/src/Scala99Spec.scala
@@ -4,12 +4,38 @@ import org.scalatest.matchers.ShouldMatchers
 import Scala99Impl._
 
 class Scala99Spec extends FlatSpec with ShouldMatchers {
+
   it should "Find the last element of a list" in {
     last(List(1, 1, 2, 3, 5, 8)) should be (8)
     intercept[Error]( last(Nil) )
   }
 
-  it should " Find the last but one element of a list" in {
+  it should "Find the last but one element of a list" in {
     penultimate(List(1, 1, 2, 3, 5, 8)) should be (5)
+    intercept[NoSuchElementException]( penultimate(Nil) )
+    intercept[NoSuchElementException]( penultimate( List(1) ) )
   }
+
+  it should "Find the Kth element of a list" in {
+    nth(2, List(1, 1, 2, 3, 5, 8)) should be (2)
+  }
+
+  it should "Find the number of elements of a list" in {
+    Scala99Impl.length(List(1, 1, 2, 3, 5, 8)) should be (6)
+  }
+
+  it should "Reverse a list" in {
+    reverse(List(1, 1, 2, 3, 5, 8)) should be ( List(8, 5, 3, 2, 1, 1) )
+  }
+
+  it should "Find out whether a list is a palindrome" in {
+    isPalindrome(List(1, 2, 3, 2, 1)) should be ( true )
+  }
+
+  it should "Flatten a nested list structure" in {
+    flatten(List(List(1, 1), 2, List(3, List(5, 8)))) should be ( List(1, 1, 2, 3, 5, 8) )
+  }
+
+
+
 }
